46f28855bb6d514b22d293476f7317a63fdaccf0
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-03-23 08:56:29 -0600

    Built out the favorites action

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,17 +4,22 @@ import { useParams, useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { deleteMovie } from '../actions/movieActions';
+import { addFavorite } from '../actions/favoritesActions';
 
 const Movie = (props) => {
     const { id } = useParams();
     const { push } = useHistory();
 
-    const {movies, displayFavorites, deleteMovie} = props;
+    const {movies, displayFavorites, deleteMovie, addFavorite} = props;
     const movie = movies.find(movie=>movie.id===Number(id));
 
     const handleDeleteClick = () => {
         deleteMovie(movie.id);
         push('/movies');
+    }
+
+    const handleFavoriteClick = () => {
+        addFavorite(movie)
     }
     
     return(<div className="modal-page col">
@@ -46,7 +51,7 @@ const Movie = (props) => {
                         </section>
                         
                         <section>
-                            <span className="m-2 btn btn-dark">Favorite</span>
+                            {displayFavorites && <span onClick={handleFavoriteClick} className="m-2 btn btn-dark">Favorite</span>}
                             <span className="delete" onClick={handleDeleteClick}><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
                         </section>
                     </div>
@@ -64,4 +69,4 @@ const mapStateToProps = state => {
     })
 }
 
-export default connect(mapStateToProps, {deleteMovie})(Movie);
+export default connect(mapStateToProps, {deleteMovie, addFavorite})(Movie);
--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { toggleFavorites } from '../actions/favoritesActions';
 import { connect } from 'react-redux';
 
 const MovieHeader = (props) => {
     const {appTitle} = props;
-    const {displayFavorites} = props;
+    const {displayFavorites, toggleFavorites} = props;
+
+const handleClick = () => {
+    toggleFavorites();
+}
+
     
     return(<div className="table-title">
         <div className="row">
@@ -13,7 +19,7 @@ const MovieHeader = (props) => {
             <h2>{appTitle}</h2>
         </div>
         <div className="col-sm-6 headerBar">
-            <div className="btn btn-sm btn-primary"><span>{ displayFavorites ? "Hide" : "Show"} Favorites</span></div>
+            <div onClick={handleClick}className="btn btn-sm btn-primary"><span>{ displayFavorites ? "Hide" : "Show"} Favorites</span></div>
             <Link to="/movies" className="btn btn-sm btn-primary">View All Movies</Link>
             <Link to="/movies/add" className="btn btn-sm btn-success"><i className="material-icons">&#xE147;</i> <span>Add New Movie</span></Link>
         </div>
@@ -28,4 +34,4 @@ const mapStateToProps = state => {
     })
 }
 
-export default connect(mapStateToProps)(MovieHeader);
+export default connect(mapStateToProps, { toggleFavorites })(MovieHeader);
